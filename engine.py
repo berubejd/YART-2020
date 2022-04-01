a76a37f2f16310cc513277b5e6b2dd705adf3a71
Jeffrey Berube <berubejd@gmail.com>
2022-04-01 05:28:07 -0500

    Completed part 4

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Any, Iterable, Set
 
 from tcod.console import Console
 from tcod.context import Context
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -22,6 +23,7 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -32,11 +34,25 @@ class Engine:
 
             action.perform(engine=self, entity=self.player)
 
+            self.update_fov()
+
+    def update_fov(self) -> None:
+        """Recompute the field of view of the player"""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+
+        # Add visible tiles to the explored tile list
+        self.game_map.explored |= self.game_map.visible
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console=console)
 
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
         console.clear()
--- tile_types.py
+++ tile_types.py
@@ -14,9 +14,10 @@ graphic_dt = np.dtype(
 # Tile struct used for statically defined tile data.
 tile_dt = np.dtype(
     [
-        ("walkable", bool),  # True if this tile can be walked over.
-        ("transparent", bool),  # True if this tile doesn't block FOV.
-        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("walkable", bool),  # True if this tile can be walked over
+        ("transparent", bool),  # True if this tile doesn't block FOV
+        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV
+        ("light", graphic_dt),  # Graphics for when this tile is in FOV
     ]
 )
 
@@ -25,29 +26,33 @@ def new_tile(
     *,  # Force use of keyword arguments
     walkable: bool,
     transparent: bool,
-    dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
+    dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:  # type: ignore
     """Helper function for defining individual tile types"""
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
     walkable=True,
     transparent=True,
     dark=(ord("."), (100, 100, 100), (0, 0, 0)),
-    # light=(ord("."), (200, 200, 200), (0, 0, 0)),
+    light=(ord("."), (200, 200, 200), (0, 0, 0)),
 )
 
 wall = new_tile(
     walkable=False,
     transparent=False,
     dark=(ord("#"), (100, 100, 100), (0, 0, 0)),
-    # light=(ord("#"), (200, 200, 200), (0, 0, 0)),
+    light=(ord("#"), (200, 200, 200), (0, 0, 0)),
 )
 
 down_stairs = new_tile(
     walkable=True,
     transparent=True,
     dark=(ord(">"), (100, 100, 100), (0, 0, 0)),
-    # light=(ord(">"), (200, 200, 200), (0, 0, 0)),
+    light=(ord(">"), (200, 200, 200), (0, 0, 0)),
 )
